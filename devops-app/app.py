131ca199c511b6868900d217cf8c2c623183bcec
surya-v52 <devtest14323@gmail.com>
2025-02-05 23:40:33 +0530

    Update app.py

--- devops-app/app.py
+++ devops-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps! Your app is running 🚀"
+    return "Hello, DevOps Engineer! Your app is running 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
